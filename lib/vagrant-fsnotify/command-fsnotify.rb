50ebbd2d6a3a600bb6f47e0f514631e2fb40202d
agung prasetyo <brain64bit@gmail.com>
2019-08-30 01:22:16 +0700

    Prevent errors on watching symlink directories
    - This will opt out error `** ERROR: directory is already being watched! **` when you run `vagrant fsnotify`

--- lib/vagrant-fsnotify/command-fsnotify.rb
+++ lib/vagrant-fsnotify/command-fsnotify.rb
@@ -1,3 +1,14 @@
+require 'listen/record/symlink_detector'
+module Listen
+  class Record
+    class SymlinkDetector
+      def _fail(_, _)
+        fail Error, "Don't watch locally-symlinked directory twice"
+      end
+    end
+  end
+end
+
 require 'listen'
 
 module VagrantPlugins::Fsnotify
